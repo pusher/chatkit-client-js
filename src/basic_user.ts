fcef9eb2660f76bb7b0e1ce56f99a6864513ae59
Callum Oakley <c.oakley108@gmail.com>
2018-02-05 10:37:23 +0000

    blank slate

--- src/basic_user.ts
+++
@@ -1,7 +0,0 @@
-interface BasicUser {
-  id: string;
-  createdAt: string;
-  updatedAt: string;
-}
-
-export default BasicUser;
--- src/constants.ts
+++
@@ -1,2 +0,0 @@
-export const TYPING_REQ_TTL = 1500;
-export const TYPING_REQ_LEEWAY = 500;

833759fce9e1206eabadbd66dfb978f50b6a99c1
Callum Oakley <c.oakley108@gmail.com>
2018-02-05 10:37:23 +0000

    blank slate

--- src/presence_payload.ts
+++
@@ -1,9 +0,0 @@
-import PresenceState from './presence_state';
-
-interface PresencePayload {
-  userId: string;
-  state: PresenceState;
-  lastSeenAt?: string;
-}
-
-export default PresencePayload;

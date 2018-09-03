7a36d6ccbfc74c291218e5e053f765d482085acf
Hamilton Chapman <hamchapman@gmail.com>
2018-09-03 15:58:56 +0100

    Add .travis.yml and tweak example.js test config

--- tests/config/example.js
+++ tests/config/example.js
@@ -1,3 +1,3 @@
-export const INSTANCE_KEY = ''
-export const INSTANCE_LOCATOR = ''
-export const TOKEN_PROVIDER_URL = ''
+export const INSTANCE_KEY = 'your:key'
+export const INSTANCE_LOCATOR = 'your:instance:locator'
+export const TOKEN_PROVIDER_URL = 'https://token.provider.url'

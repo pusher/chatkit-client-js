efc2296d21456aba89617189d49fb9f186366aac
Callum Oakley <c.oakley108@gmail.com>
2018-10-31 15:32:48 +0000

    disable eslint for test config

--- tests/config/example.js
+++ tests/config/example.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 export const INSTANCE_KEY = "your:key"
 export const INSTANCE_LOCATOR = "your:instance:locator"
 export const TOKEN_PROVIDER_URL = "https://token.provider.url"

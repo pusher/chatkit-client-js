042e52c4175687293057d93c153aaa03cc56ff79
Callum Oakley <c.oakley108@gmail.com>
2019-01-08 13:56:14 +0000

    failing user sub reconnection tests

--- tests/unit/reconnection-handlers.js
+++
@@ -1,8 +0,0 @@
-import test from "tape"
-
-import { handleUserSubReconnection } from "../../src/reconnection-handlers.js"
-
-test("hello", t => {
-  t.equal(typeof handleUserSubReconnection, "function")
-  t.end()
-})
---
+++ tests/unit/user-sub-reconnection.js
@@ -0,0 +1,343 @@
+import test from "tape"
+
+import { RoomStore } from "../../src/room-store.js"
+import { handleUserSubReconnection } from "../../src/reconnection-handlers.js"
+
+const TEST_TIMEOUT = 200
+
+const roomStoreRooms = [
+  {
+    id: "1",
+    created_by_id: "ham",
+    name: "one",
+    private: false,
+    created_at: "2017-04-13T14:10:38Z",
+    updated_at: "2017-04-13T14:10:38Z",
+  },
+  {
+    id: "2",
+    created_by_id: "ham",
+    name: "two",
+    private: false,
+    created_at: "2017-04-13T14:10:38Z",
+    updated_at: "2017-04-13T14:10:38Z",
+  },
+  {
+    id: "3",
+    created_by_id: "ham",
+    name: "three",
+    private: false,
+    created_at: "2017-04-13T14:10:38Z",
+    updated_at: "2017-04-13T14:10:38Z",
+  },
+  {
+    id: "4",
+    created_by_id: "ham",
+    name: "four",
+    private: false,
+    created_at: "2017-04-13T14:10:38Z",
+    updated_at: "2017-04-13T14:10:38Z",
+  },
+  {
+    id: "5",
+    created_by_id: "ham",
+    name: "five",
+    private: false,
+    created_at: "2017-04-13T14:10:38Z",
+    updated_at: "2017-04-13T14:10:38Z",
+  },
+  {
+    id: "7",
+    created_by_id: "ham",
+    name: "seven",
+    custom_data: { pre: "set", custom: "data" },
+    private: false,
+    created_at: "2017-04-13T14:10:38Z",
+    updated_at: "2017-04-13T14:10:38Z",
+  },
+  {
+    id: "8",
+    created_by_id: "ham",
+    name: "eight",
+    custom_data: { pre: "set" },
+    private: false,
+    created_at: "2017-04-13T14:10:38Z",
+    updated_at: "2017-04-13T14:10:38Z",
+  },
+  {
+    id: "9",
+    created_by_id: "ham",
+    name: "nine",
+    custom_data: { pre: "set" },
+    private: false,
+    created_at: "2017-04-13T14:10:38Z",
+    updated_at: "2017-04-13T14:10:38Z",
+  },
+]
+
+const roomsData = [
+  {
+    id: "1",
+    created_by_id: "ham",
+    name: "one",
+    private: false,
+    created_at: "2017-04-13T14:10:38Z",
+    updated_at: "2017-04-13T14:10:38Z",
+  },
+  {
+    id: "3",
+    created_by_id: "ham",
+    name: "three",
+    private: true,
+    created_at: "2017-04-13T14:10:38Z",
+    updated_at: "2017-04-13T14:10:38Z",
+  },
+  {
+    id: "4",
+    created_by_id: "ham",
+    name: "four",
+    private: false,
+    custom_data: { set: "now" },
+    created_at: "2017-04-13T14:10:38Z",
+    updated_at: "2017-04-13T14:10:38Z",
+  },
+  {
+    id: "5",
+    created_by_id: "ham",
+    name: "5ive",
+    private: false,
+    created_at: "2017-04-13T14:10:38Z",
+    updated_at: "2017-04-13T14:10:38Z",
+  },
+  {
+    id: "6",
+    created_by_id: "ham",
+    name: "size",
+    private: false,
+    created_at: "2017-04-13T14:10:38Z",
+    updated_at: "2017-04-13T14:10:38Z",
+  },
+  {
+    id: "7",
+    created_by_id: "ham",
+    name: "seven",
+    custom_data: { pre: "set", custom: "data", third: "field" },
+    private: false,
+    created_at: "2017-04-13T14:10:38Z",
+    updated_at: "2017-04-13T14:10:38Z",
+  },
+  {
+    id: "8",
+    created_by_id: "ham",
+    name: "eight",
+    private: false,
+    created_at: "2017-04-13T14:10:38Z",
+    updated_at: "2017-04-13T14:10:38Z",
+  },
+  {
+    id: "9",
+    created_by_id: "ham",
+    name: "9ine",
+    custom_data: { pre: "set", and: "updated" },
+    private: true,
+    created_at: "2017-04-13T14:10:38Z",
+    updated_at: "2017-04-13T14:10:38Z",
+  },
+]
+
+test("room removed", t => {
+  const roomStore = new RoomStore({})
+  roomStoreRooms.forEach(room => roomStore.set(room))
+
+  const onRemovedFromRoom = room => {
+    if (room.id != "2") {
+      return
+    }
+    t.equal(room.createdByUserId, "ham")
+    t.equal(room.name, "two")
+    t.equal(room.isPrivate, false)
+    t.end()
+  }
+
+  handleUserSubReconnection({
+    roomsData,
+    roomStore,
+    hooks: { global: { onRemovedFromRoom } },
+  })
+
+  t.timeoutAfter(TEST_TIMEOUT)
+})
+
+test("privacy changed", t => {
+  const roomStore = new RoomStore({})
+  roomStoreRooms.forEach(room => roomStore.set(room))
+
+  const onRoomUpdated = room => {
+    if (room.id != "3") {
+      return
+    }
+    t.equal(room.createdByUserId, "ham")
+    t.equal(room.name, "three")
+    t.equal(room.isPrivate, true)
+    t.end()
+  }
+
+  handleUserSubReconnection({
+    roomsData,
+    roomStore,
+    hooks: { global: { onRoomUpdated } },
+  })
+
+  t.timeoutAfter(TEST_TIMEOUT)
+})
+
+test("custom data added", t => {
+  const roomStore = new RoomStore({})
+  roomStoreRooms.forEach(room => roomStore.set(room))
+
+  const onRoomUpdated = room => {
+    if (room.id != "4") {
+      return
+    }
+    t.equal(room.createdByUserId, "ham")
+    t.equal(room.name, "four")
+    t.equal(room.isPrivate, false)
+    t.deepEqual(room.customData, { set: "now" })
+    t.end()
+  }
+
+  handleUserSubReconnection({
+    roomsData,
+    roomStore,
+    hooks: { global: { onRoomUpdated } },
+  })
+
+  t.timeoutAfter(TEST_TIMEOUT)
+})
+
+test("custom data updated", t => {
+  const roomStore = new RoomStore({})
+  roomStoreRooms.forEach(room => roomStore.set(room))
+
+  const onRoomUpdated = room => {
+    if (room.id != "7") {
+      return
+    }
+    t.equal(room.createdByUserId, "ham")
+    t.equal(room.name, "seven")
+    t.equal(room.isPrivate, false)
+    t.deepEqual(room.customData, { pre: "set", custom: "data", third: "field" })
+    t.end()
+  }
+
+  handleUserSubReconnection({
+    roomsData,
+    roomStore,
+    hooks: { global: { onRoomUpdated } },
+  })
+
+  t.timeoutAfter(TEST_TIMEOUT)
+})
+
+test("custom data removed", t => {
+  const roomStore = new RoomStore({})
+  roomStoreRooms.forEach(room => roomStore.set(room))
+
+  const onRoomUpdated = room => {
+    if (room.id != "8") {
+      return
+    }
+    t.equal(room.createdByUserId, "ham")
+    t.equal(room.name, "eight")
+    t.equal(room.isPrivate, false)
+    t.equal(room.customData, undefined)
+    t.end()
+  }
+
+  handleUserSubReconnection({
+    roomsData,
+    roomStore,
+    hooks: { global: { onRoomUpdated } },
+  })
+
+  t.timeoutAfter(TEST_TIMEOUT)
+})
+
+test("name changed", t => {
+  const roomStore = new RoomStore({})
+  roomStoreRooms.forEach(room => roomStore.set(room))
+
+  const onRoomUpdated = room => {
+    if (room.id != "5") {
+      return
+    }
+    t.equal(room.createdByUserId, "ham")
+    t.equal(room.name, "5ive")
+    t.equal(room.isPrivate, false)
+    t.end()
+  }
+
+  handleUserSubReconnection({
+    roomsData,
+    roomStore,
+    hooks: { global: { onRoomUpdated } },
+  })
+
+  t.timeoutAfter(TEST_TIMEOUT)
+})
+
+test("multiple field changes (only one event!)", t => {
+  const roomStore = new RoomStore({})
+  roomStoreRooms.forEach(room => roomStore.set(room))
+
+  let called = false
+
+  const onRoomUpdated = room => {
+    if (room.id != "9") {
+      return
+    }
+    t.equal(room.createdByUserId, "ham")
+    t.equal(room.name, "9ine")
+    t.equal(room.isPrivate, true)
+    t.deepEqual(room.customData, { pre: "set", and: "updated" })
+    if (called) {
+      t.end("onRoomUpdated called more than once")
+      return
+    }
+    called = true
+    setTimeout(() => t.end(), 100)
+  }
+
+  handleUserSubReconnection({
+    roomsData,
+    roomStore,
+    hooks: { global: { onRoomUpdated } },
+  })
+
+  t.timeoutAfter(TEST_TIMEOUT)
+})
+
+test("room added", t => {
+  const roomStore = new RoomStore({})
+  roomStoreRooms.forEach(room => roomStore.set(room))
+
+  const onRoomUpdated = room => {
+    if (room.id != "6") {
+      return
+    }
+    t.equal(room.createdByUserId, "ham")
+    t.equal(room.name, "size")
+    t.equal(room.isPrivate, false)
+    t.end()
+  }
+
+  handleUserSubReconnection({
+    roomsData,
+    roomStore,
+    hooks: { global: { onRoomUpdated } },
+  })
+
+  t.timeoutAfter(TEST_TIMEOUT)
+})
+
+// TODO current user changes

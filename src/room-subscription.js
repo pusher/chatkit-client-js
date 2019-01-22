960c07a4615b20d6db7933dccbe779ff0670245c
Callum Oakley <c.oakley108@gmail.com>
2019-01-22 14:08:27 +0000

    have RoomSubscription co-ordinate the creation of its parts

--- src/room-subscription.js
+++ src/room-subscription.js
@@ -1,8 +1,49 @@
+import { CursorSubscription } from "./cursor-subscription"
+import { MessageSubscription } from "./message-subscription"
+import { MembershipSubscription } from "./membership-subscription"
+
 export class RoomSubscription {
   constructor(options) {
-    this.messageSub = options.messageSub
-    this.cursorSub = options.cursorSub
-    this.membershipSub = options.membershipSub
+    this.messageSub = new MessageSubscription({
+      roomId: options.roomId,
+      hooks: options.hooks,
+      messageLimit: options.messageLimit,
+      userId: options.userId,
+      instance: options.apiInstance,
+      userStore: options.userStore,
+      roomStore: options.roomStore,
+      typingIndicators: options.typingIndicators,
+      logger: options.logger,
+      connectionTimeout: options.connectionTimeout,
+    })
+
+    this.cursorSub = new CursorSubscription({
+      onNewCursorHook: cursor => {
+        if (
+          options.hooks.rooms[options.roomId] &&
+          options.hooks.rooms[options.roomId].onNewReadCursor &&
+          cursor.type === 0 &&
+          cursor.userId !== options.userId
+        ) {
+          options.hooks.rooms[options.roomId].onNewReadCursor(cursor)
+        }
+      },
+      path: `/cursors/0/rooms/${encodeURIComponent(options.roomId)}`,
+      cursorStore: options.cursorStore,
+      instance: options.cursorsInstance,
+      logger: options.logger,
+      connectionTimeout: options.connectionTimeout,
+    })
+
+    this.membershipSub = new MembershipSubscription({
+      roomId: options.roomId,
+      hooks: options.hooks,
+      instance: options.apiInstance,
+      userStore: options.userStore,
+      roomStore: options.roomStore,
+      logger: options.logger,
+      connectionTimeout: options.connectionTimeout,
+    })
   }
 
   connect() {
